cb20ce66c1a0194fb9498002626b5e21e340b050
carldumas <dumas.carl@gmail.com>
2020-07-10 20:23:10 -0400

    M3-Project: Request the server to get seating availability for a flight

--- server.js
+++ server.js
@@ -8,28 +8,29 @@ const { flights } = require('./test-data/flightSeating');
 const PORT = process.env.PORT || 8000;
 
 const handleFlight = (req, res) => {
-  const { flightNumber } = req.params;
-  // get all flight numbers
-  const allFlights = Object.keys(flights);
-  // is flightNumber in the array?
-  console.log('REAL FLIGHT: ', allFlights.includes(flightNumber));
+    const { flightNumber } = req.params;
+    // get all flight numbers
+    const allFlights = Object.keys(flights);
+    // is flightNumber in the array?
+    res.json(flights[flightNumber])
+    console.log('REAL FLIGHT: ', allFlights.includes(flightNumber));
 };
 
 express()
-  .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  })
-  .use(morgan('dev'))
-  .use(express.static('public'))
-  .use(bodyParser.json())
-  .use(express.urlencoded({ extended: false }))
+    .use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+        );
+        next();
+    })
+    .use(morgan('dev'))
+    .use(express.static('public'))
+    .use(bodyParser.json())
+    .use(express.urlencoded({ extended: false }))
 
-  // endpoints
-  .get('/flights/:flightNumber', handleFlight)
-  .use((req, res) => res.send('Not Found'))
-  .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    // endpoints
+    .get('/flights/:flightNumber', handleFlight)
+    .use((req, res) => res.send('Not Found'))
+    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
